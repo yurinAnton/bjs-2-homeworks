7b36c0062caf00da43e4ce4bfaf23c3fdd6cdeeb
Anton Yurin <kantona83@rambler.ru>
2022-11-29 09:50:27 +0300

    Second variant added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,46 +1,52 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  let x1;
-  let x2;
-  let d = b**2 - 4*a*c;
+   let arr = [];
+   let x1;
+   let x2;
+   let d = b ** 2 - 4 * a * c;
 
-  if (d > 0) {
-     x1 = (-b + Math.sqrt(d)) / (2 * a);
-     x2 = (-b - Math.sqrt(d)) / (2 * a);
-     arr.push(x1);
-     arr.push(x2);
-     return arr;
-  } else if ((d = 0)) {
-     x1 = -b / (2 * a);
-     arr.push(x1);
-     return arr;
-  } else return arr;
+   if (d > 0) {
+      x1 = (-b + Math.sqrt(d)) / (2 * a);
+      x2 = (-b - Math.sqrt(d)) / (2 * a);
+      arr.push(x1);
+      arr.push(x2);
+      return arr;
+   } else if ((d = 0)) {
+      x1 = -b / (2 * a);
+      arr.push(x1);
+      return arr;
+   } else return arr;
 }
 
 // solveEquation(1, 5, 4);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let payment;
-  let sumCount;
-  let monthPer = percent / 100 / 12;
-  let bodyCount = amount - contribution;
+   let payment;
+   let sumCount;
+   let monthPer = percent / 100 / 12;
+   let bodyCount = amount - contribution;
+   let err;
+   let errorMessage = `Параметр ${err} содержит
+   не правильное значение ${typeof err}!`;
 
-  if (
-     isNaN(percent) ||
-     isNaN(contribution) ||
-     isNaN(amount) ||
-     isNaN(countMonths)
-  ) {
-     return console.log(`Параметр ${percent} содержит 
-     не правильное значение ${typeof(percent)}!`);
-
-  }
-
-  payment =
-     bodyCount * (monthPer + monthPer / ((1 + monthPer) ** countMonths - 1));
-  sumCount = parseFloat((payment * countMonths).toFixed(2));
-  return sumCount;
+   if (isNaN(percent)) {
+      err = percent;
+      console.log(errorMessage);
+   } else if (isNaN(contribution)) {
+      err = contribution;
+      console.log(errorMessage);
+   } else if (isNaN(amount)) {
+      err = amount;
+      console.log(errorMessage);
+   } else if (isNaN(countMonths)) {
+      err = countMonths;
+      console.log(errorMessage);
+   } else
+      payment =
+         bodyCount *
+         (monthPer + monthPer / ((1 + monthPer) ** countMonths - 1));
+   sumCount = parseFloat((payment * countMonths).toFixed(2));
+   return sumCount;
 }
 
 // calculateTotalMortgage(10, 0, 50000, 12);
